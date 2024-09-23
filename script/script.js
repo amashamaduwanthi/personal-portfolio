9f5cd27c2a85abbbf16bf50f1738f913e6fb8d31
amashamaduwanthi <amashamaduwanthi123@gmail.com>
2024-09-23 10:13:25 +0530

    typed js

--- script/script.js
+++ script/script.js
@@ -38,3 +38,14 @@ ScrollReveal().reveal('.home-content,heading', { origin: 'top' });
 ScrollReveal().reveal('.home-image,.services-container,portfolio-box,.contact form,.skill-box', { origin: 'bottom' });
 ScrollReveal().reveal('.home-contact h1,.about-img ', { origin: 'left' });
 ScrollReveal().reveal('.home-contact p,.about-content', { origin: 'right' });
+
+
+//typed js
+const typed=new Typed('.multiple-text',{
+strings:['Fullstack Developer','Web Designer'],
+    typeSpeed:70,
+    backSpeed:70,
+    backDelay:1000,
+    loop:true
+
+})
